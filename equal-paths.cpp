39095a045782aab49b96b414e4a56a52ff235837
Wing Nie <wcnie@usc.edu>
2022-11-04 21:45:20 +0000

    Tree Traversals

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -3,11 +3,31 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int equalHelper(Node* root, bool& equal, int length, int temp){
+    if(root == nullptr) return 0;
+    if(!root->left && !root->right && temp != length && temp != 0) {
+        equal = false;
+        return length;
+    }
+    else if (!root->left && !root->right && !(temp != length && temp != 0)) {
+        temp = length;
+        return length;
+    }
+    else {
+        length++;
+        temp = equalHelper(root->left, equal, length, temp);
+        temp = equalHelper(root->right, equal, length, temp);
+    }
+    return 0;
+}
+
 
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    bool equal = true;
+    equalHelper(root, equal, 0, 0);
+    return equal; 
 }
 
